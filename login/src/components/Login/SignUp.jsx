980ece278b21d01a0456cc08e426876a15e202ec
ayushjidhanta <ayushjidhanta@gmail.com>
2023-04-07 19:19:29 +0530

    Repos Chnaged

--- login/src/components/Login/SignUp.jsx
+++ login/src/components/Login/SignUp.jsx
@@ -12,7 +12,7 @@ const SignUp = () => {
     password: "",
     confirmpassword: "",
   });
-  
+
   const handleAuth = async (e) => {
     e.preventDefault();
     // console.log("The signup is Cicked "); // check----
@@ -29,11 +29,15 @@ const SignUp = () => {
     //   localStorage.setItem("passwordP", pass);
     //   navigate("/");
     //   console.log("Navigation to login working--")
-    // } 
+    // }
 
-    if (credentials.name === '' || credentials.password === '' || credentials.confirmpassword === '') {
-        console.log("Something is empty...");
-        return;
+    if (
+      credentials.name === "" ||
+      credentials.password === "" ||
+      credentials.confirmpassword === ""
+    ) {
+      console.log("Something is empty...");
+      return;
     }
 
     if (credentials.password !== credentials.confirmpassword) {
@@ -44,30 +48,46 @@ const SignUp = () => {
     try {
       const response = await signupuser(credentials);
       console.log(response);
+      navigate("/");
     } catch (error) {
       console.log("error", error);
     }
-
-
   };
 
   const onChange = (e) => {
-    setcredentials({...credentials, [e.target.name] : e.target.value});
-
-  }
+    setcredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
 
   return (
-    <div className = "page">
-    <div className="coverSignup">
-      {/* <div className="wrapper"> */}
-      <form onSubmit={handleAuth} method="POST">
-        <h1> Signup </h1>
-        <input type="text" id="user" placeholder="username" name ="name"onChange={onChange}/>
-        <input type="password" id="pass" placeholder="password" name = "password"onChange={onChange}/>
-        <input type="password" id="pass2" placeholder="confirm password" name ="confirmpassword" onChange={onChange}/>
-        <button type="submit" className="signup-btn" >
-          Signup
-        </button>
+    <div className="page">
+      <div className="coverSignup">
+        {/* <div className="wrapper"> */}
+        <form onSubmit={handleAuth} method="POST">
+          <h1> Signup </h1>
+          <input
+            type="text"
+            id="user"
+            placeholder="username"
+            name="name"
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            id="pass"
+            placeholder="password"
+            name="password"
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            id="pass2"
+            placeholder="confirm password"
+            name="confirmpassword"
+            onChange={onChange}
+          />
+          <button type="submit" className="signup-btn">
+            Signup
+          </button>
         </form>
       </div>
     </div>
